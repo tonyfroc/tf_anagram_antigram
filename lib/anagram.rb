d6f9e6f794585064e6aa508ec1a006ff630ccb7b
tonyfroc <tfroccaro@gmail.com>
2022-04-23 00:20:57 -0400

    Test 2 Success: compare_length method checks if arrays are the same length

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,8 +1,21 @@
 require('pry')
 
 class Anagram
-  def variables(word1, word2)
-    @word1 = word1.downcase().chars().sort(&:casecmp).join().split("")
-    @word2 = word2.downcase().chars().sort(&:casecmp).join().split("")
+  def initialize(word1, word2)
+    @word1 = word1
+    @word2 = word2
   end
+
+  def mod_string(word)
+    word.downcase().chars().sort(&:casecmp).join().split("")
+  end
+
+  def compare_length()
+    if @word1.length == @word2.length
+      true
+    else
+      "These words are not anagrams because they are not the same length."
+    end
+  end
+
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,10 +3,16 @@ require('spec_helper')
 require('anagram')
 
 describe (Anagram) do
-  describe ('#variables') do
+  describe ('#mod_string') do
     it("downcases and splits the string, listing the letters alphabetically") do
-    test = Anagram.new
-    expect(test.variables("aBaC","ABAC")).to(eq(["a","a","b","c"]))
+    word = Anagram.new("aBaC", "ABAC")
+    expect(word.mod_string("aBaC")).to(eq(["a","a","b","c"]))
     end
   end
+  describe ('#check_length') do
+    it("downcases and splits the string, listing the letters alphabetically") do
+    word = Anagram.new("aBaC", "ABACd")
+    expect(word.compare_length()).to(eq("These words are not anagrams because they are not the same length."))
+    end
+end
 end
