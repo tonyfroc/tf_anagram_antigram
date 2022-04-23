30cc34cf077fc4f4bcc591f4507c8bfdc5bd6511
tonyfroc <tfroccaro@gmail.com>
2022-04-22 22:17:48 -0400

    First test that tests if the method downcases, and lists the string alphabetically

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,5 +1,12 @@
-require 'rspec'
-require 'spec_helper'
-require 'anagram'
+require('rspec')
+require('spec_helper')
+require('anagram')
 
-# Tests go here
+describe (Anagram) do
+  describe ('#mod_string') do
+  it("downcases and rearranges a string, listing the string in alphabetical order") do
+    test = Anagram.new
+    expect(test.mod_string("abac")).to(eq("aabc"))
+  end
+end
+end
