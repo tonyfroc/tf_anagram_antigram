e7de6d7e0103ad6a7d6e22284df8f1117e3ad9a5
tonyfroc <tfroccaro@gmail.com>
2022-04-23 18:26:35 -0400

    Added logic for check_vowels and is_anagram?

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,20 +2,47 @@ require('pry')
 
 class Anagram
   def initialize(word1, word2)
-    @word1 = word1
-    @word2 = word2
-  end
-
-  def mod_string(word)
-    word.downcase().chars().sort(&:casecmp).join().split("")
+    @word1 = word1.gsub(/\s+/, "").downcase().chars().sort(&:casecmp).join().split("")
+    @word2 = word2.gsub(/\s+/, "").downcase().chars().sort(&:casecmp).join().split("")
   end
 
   def compare_length()
     if @word1.length == @word2.length
       true
     else
-      "These words are not anagrams because they are not the same length."
+      false
     end
   end
+
+  def check_vowels()
+    vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+    word_one_counter = 0
+    word_two_counter = 0
+    @word1.each do |letter|
+      if vowels.include?(letter)
+        word_one_counter += 1
+      end
+    end
+    @word2.each do |letter|
+      if vowels.include?(letter)
+        word_two_counter += 1
+      end
+    end
+    if word_one_counter >= 1 && word_two_counter >= 1
+      true
+    else
+      false
+  end
+end
+
+  def is_anagram?()
+    if compare_length == true && check_vowels == true
+      if @word1 == @word2
+        "ANAGRAM CONFIRMED!"
+      end
+      else
+      "ANAGRAM DENIED!"
+  end
+end
 
 end
