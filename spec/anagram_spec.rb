c8b5d801b9464af72abc875d045fb4aef7878ff4
tonyfroc <tfroccaro@gmail.com>
2022-04-22 22:28:24 -0400

    Removed test 1 because addition logic to test 2 broke it. Test 2 now passes and splits the string

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -4,10 +4,6 @@ require('anagram')
 
 describe (Anagram) do
   describe ('#mod_string') do
-  it("downcases and rearranges a string, listing the string in alphabetical order") do
-    test = Anagram.new
-    expect(test.mod_string("aBaC")).to(eq("aabc"))
-  end
   it("downcases and splits the string, listing the letters alphabetically") do
     test = Anagram.new
     expect(test.mod_string("aBaC")).to(eq(["a","a","b","c"]))
