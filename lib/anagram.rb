85306cb252e3b0f8f2df1867c2a2e880fb1e48a5
tonyfroc <tfroccaro@gmail.com>
2022-04-22 23:08:14 -0400

    Re-factored previous tests to make variables that downcase a string, list it alphabetically, then split it

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,8 @@
 require('pry')
 
 class Anagram
-  def mod_string(str)
-    str.downcase().chars().sort(&:casecmp).join().split("")
+  def variables(word1, word2)
+    @word1 = word1.downcase().chars().sort(&:casecmp).join().split("")
+    @word2 = word2.downcase().chars().sort(&:casecmp).join().split("")
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,10 +3,10 @@ require('spec_helper')
 require('anagram')
 
 describe (Anagram) do
-  describe ('#mod_string') do
-  it("downcases and splits the string, listing the letters alphabetically") do
+  describe ('#variables') do
+    it("downcases and splits the string, listing the letters alphabetically") do
     test = Anagram.new
-    expect(test.mod_string("aBaC")).to(eq(["a","a","b","c"]))
+    expect(test.variables("aBaC","ABAC")).to(eq(["a","a","b","c"]))
+    end
   end
-end
 end
