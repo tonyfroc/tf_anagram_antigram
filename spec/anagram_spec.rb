e10a930f4426e5426909e58311adedb73b131a58
tonyfroc <tfroccaro@gmail.com>
2022-04-22 22:26:29 -0400

    Test 2 Fail: The method mod_string does not split the string

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -6,7 +6,11 @@ describe (Anagram) do
   describe ('#mod_string') do
   it("downcases and rearranges a string, listing the string in alphabetical order") do
     test = Anagram.new
-    expect(test.mod_string("abac")).to(eq("aabc"))
+    expect(test.mod_string("aBaC")).to(eq("aabc"))
+  end
+  it("downcases and splits the string, listing the letters alphabetically") do
+    test = Anagram.new
+    expect(test.mod_string("aBaC")).to(eq(["a","a","b","c"]))
   end
 end
 end
