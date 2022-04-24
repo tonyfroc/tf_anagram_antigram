f9c54734f4c4cf3bbd6cf09d9c1c0c50d09f8cea
tonyfroc <tfroccaro@gmail.com>
2022-04-23 21:15:42 -0400

    Added tests to account for remaining anagram checks

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -18,15 +18,31 @@ describe (Anagram) do
       expect(word.check_vowels()).to(eq(true))
         end
   describe ('#is_anagram?') do
-    it("returns correct string for two words that are not anagrams") do
+    it("confirms words are anagrams of one another despite case") do
+      word = Anagram.new("TEA", "eat")
+      expect(word.is_anagram?()).to(eq("ANAGRAM CONFIRMED!"))
+        end
+    it("returns NO ACTUAL WORDS DETECTED! for words with no vowels") do
+      word = Anagram.new("pkmn", "kmpn")
+      expect(word.is_anagram?()).to(eq("NO ACTUAL WORDS DETECTED!"))
+        end
+    it("accounts for two sentences being compared as anagrams") do
+      word = Anagram.new("The Morse Code", "Here come dots")
+      expect(word.is_anagram?()).to(eq("ANAGRAM CONFIRMED!"))
+        end
+    it("trims punctuation and whitespace before comparing words") do
+      word = Anagram.new("The Morse Code!!!", "Here come dots!!!")
+      expect(word.is_anagram?()).to(eq("ANAGRAM CONFIRMED!"))
+        end
+    it("returns ANAGRAM DENIED! for two words that are not anagrams") do
       word = Anagram.new("aBaC", "ABACe")
       expect(word.is_anagram?()).to(eq("ANAGRAM DENIED!"))
         end
-    it("returns correct string for two words that are anagrams") do
+    it("returns ANAGRAM CONFIRMED! for two words that are anagrams") do
       word = Anagram.new("RUBY", "ubry")
       expect(word.is_anagram?()).to(eq("ANAGRAM CONFIRMED!"))
         end
-    it("returns correct string for two words that are antigrams") do
+    it("returns ANAGRAM CONFIRMED! for two words that are antigrams") do
       word = Anagram.new("test", "gray")
       expect(word.is_anagram?()).to(eq("ANTIGRAM CONFIRMED!"))
         end    
