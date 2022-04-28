2730074a0367fd679805eb558f3c1a40eee1dd27
tonyfroc <tfroccaro@gmail.com>
2022-04-27 23:30:24 -0400

    Some revisions to indentation and variable names, addition of attr_reader

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Anagram and Antigrams 
+# Anagram and Antigrams
 
 #### By Tony Froccaro
 
@@ -11,7 +11,7 @@
 
 ## Description
 
-Anagrams and Antigrams checks if two words are anagrams. An anagram "...is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once." To achieve this functionality, the two arguments (strings -- words or sentences) are trimmed of white space and punctuation, downcased, sorted in alphabetical order, and finally returned in an alphabetically ordered array of letters. Next, the two arrays lengths are compared and they are scanned to check if any vowels are present. If they are the same length, and contain at least one vowel the is_anagram? method checks if the arrays are strictly equal. If they are strictly equal, they are anagrams. In addition to checking if the two strings are anagrams, if they are not anagrams, the application checks if the strings are antigrams. Antigrams do not contain a single letter that is identical. If there are no vowels present in either of the strings, the application returns "NO ACTUAL WORDS DETECTED!". If at least one word is identical in each string, but they are not anagrams, the program returns "NO ANAGRAM DETECTED!".
+Anagrams and Antigrams checks if two words are anagrams. An anagram "...is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once." To achieve this functionality, the two arguments (strings -- words or sentences) are trimmed of white space and punctuation, downcased, sorted in alphabetical order, and finally returned in an alphabetically ordered array of letters. Next, the two arrays lengths are compared and they are scanned to check if any vowels are present. If they are the same length, and contain at least one vowel the is_anagram method checks if the arrays are strictly equal. If they are strictly equal, they are anagrams. In addition to checking if the two strings are anagrams, if they are not anagrams, the application checks if the strings are antigrams. Antigrams do not contain a single letter that is identical. If there are no vowels present in either of the strings, the application returns "NO ACTUAL WORDS DETECTED!". If at least one word is identical in each string, but they are not anagrams, the program returns "NO ANAGRAM DETECTED!".
 
 ## Tests
 
@@ -33,7 +33,7 @@ See `anagram_logic_spec.rb` in the `spec` folder.
 
 ## Known Bugs
 
-- No user interface built yet. 
+- No user interface built yet.
 
 ## License
 
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,56 +3,63 @@ require('spec_helper')
 require('anagram')
 
 describe (Anagram) do
+  describe ('#initialize') do
+    it("creates an instance of Anagram that contains two attributes, @word1 and @word2") do
+      word = Anagram.new("The Morse Code", "Here come dots")
+      expect(word.word1).to(eq(["c", "d", "e", "e", "e", "h", "m", "o", "o", "r", "s", "t"]))
+      expect(word.word1).to(eq(["c", "d", "e", "e", "e", "h", "m", "o", "o", "r", "s", "t"]))
+    end
+  end
   describe ('#compare_length') do
     it("checks if the two arrays are the same length") do
       word = Anagram.new("aBaC", "ABACe")
       expect(word.compare_length()).to(eq(false))
-        end
+    end
+  end
   describe ('#check_vowels') do
     it("checks if vowels are present in the array. If none are present, return false") do
       word = Anagram.new("plkm", "plkm")
       expect(word.check_vowels()).to(eq(false))
-        end
+    end
     it("checks if vowels are present in the array. If vowels are present, return true") do
       word = Anagram.new("Hello", "Raw")
       expect(word.check_vowels()).to(eq(true))
-        end
-  describe ('#is_anagram?') do
+    end
+  end
+  describe ('#is_anagram') do
     it("confirms words are anagrams of one another despite case") do
       word = Anagram.new("TEA", "eat")
-      expect(word.is_anagram?()).to(eq("ANAGRAM DETECTED!"))
-        end
+      expect(word.is_anagram()).to(eq("ANAGRAM DETECTED!"))
+    end
     it("returns NO ACTUAL WORDS DETECTED! for words with no vowels") do
       word = Anagram.new("pkmn", "kmpn")
-      expect(word.is_anagram?()).to(eq("NO ACTUAL WORDS DETECTED!"))
-        end
+      expect(word.is_anagram()).to(eq("NO ACTUAL WORDS DETECTED!"))
+    end
     it("accounts for two sentences being compared as anagrams") do
       word = Anagram.new("The Morse Code", "Here come dots")
-      expect(word.is_anagram?()).to(eq("ANAGRAM DETECTED!"))
-        end
+      expect(word.is_anagram()).to(eq("ANAGRAM DETECTED!"))
+    end
     it("trims punctuation and whitespace before comparing words") do
       word = Anagram.new("The Morse Code!!!", "Here come dots!!!")
-      expect(word.is_anagram?()).to(eq("ANAGRAM DETECTED!"))
-        end
+      expect(word.is_anagram()).to(eq("ANAGRAM DETECTED!"))
+    end
     it("returns NO ANAGRAM DETECTED! for two words that are not anagrams") do
       word = Anagram.new("aBaC", "ABACe")
-      expect(word.is_anagram?()).to(eq("NO ANAGRAM DETECTED!"))
-        end
+      expect(word.is_anagram()).to(eq("NO ANAGRAM DETECTED!"))
+    end
     it("returns ANAGRAM CONFIRMED! for two words that are anagrams") do
       word = Anagram.new("RUBY", "ubry")
-      expect(word.is_anagram?()).to(eq("ANAGRAM DETECTED!"))
-        end
+      expect(word.is_anagram()).to(eq("ANAGRAM DETECTED!"))
+    end
     it("returns ANAGRAM CONFIRMED! for two words that are antigrams") do
       word = Anagram.new("test", "gray")
-      expect(word.is_anagram?()).to(eq("ANTIGRAM DETECTED!"))
-        end    
+      expect(word.is_anagram()).to(eq("ANTIGRAM DETECTED!"))
+    end
+  end  
   describe ('#is_antigram?') do
     it("checks if two strings are antigrams") do
       word = Anagram.new("test", "gray")
       expect(word.is_antigram?()).to(eq(true))
-        end
-      end 
     end
-  end
-end
+  end 
 end
